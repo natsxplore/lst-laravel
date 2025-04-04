30de032a2af433213f662d16b3e1b6f6b4e0c40f
Jonathan <abivajonathan17@gmail.com>
2025-04-05 04:46:29 +0800

    install jquery and blockui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
 import './bootstrap';
+import $ from 'jquery';
+import 'block-ui/jquery.blockUI.js';
+
+window.$ = window.jQuery = $;
+
+const loaderHTML = '<span class="loader"></span>';
+const blockUI = () => {
+    $.blockUI({
+        message: loaderHTML,
+        css: {
+            border: 'none',
+            backgroundColor: 'transparent'
+        },
+        overlayCSS: {
+            backgroundColor: '#000',
+            opacity: 0.7,
+            cursor: 'wait'
+        }
+    });
+};
+
+const unblockUI = () => $.unblockUI();
+
+// Auto Block UI on load and AJAX
+$(function () {
+    blockUI();
+    $(window).on('load', unblockUI);
+    $(document)
+        .ajaxStart(blockUI)
+        .ajaxStop(unblockUI)
+        .ajaxError(unblockUI);
+});
+
+window.routeTo = (url) => window.location.href = url;
